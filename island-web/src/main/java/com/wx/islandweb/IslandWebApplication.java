5c975c5b0fe3ebf4b92a751cdd28351997730609
aa792978017 <792978017@qq.com>
2018-10-02 01:41:13 +0800

    初始配置
    初始配置

---
+++ island-web/src/main/java/com/wx/islandweb/IslandWebApplication.java
@@ -0,0 +1,12 @@
+package com.wx.islandweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslandWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslandWebApplication.class, args);
+	}
+}
